39acadeb4be8e033454a993e06eeeb22cfdb61b6
Moon Choi <choimy1991@gmail.com>
2022-06-06 10:22:26 -0600

    EDD Rails Example - Part 3 Video

---
+++ app/helpers/sales_helper.rb
@@ -0,0 +1,11 @@
+# module SaleshHelper
+#   def active_sale?
+#    Sale.where("sales.starts_on <= ? AND sales.end_on >= ?", Date.current, Date.current).any? 
+#   end
+# end
+
+module SaleshHelper
+  def active_sale?
+   Sale.active.any?
+  end
+end
