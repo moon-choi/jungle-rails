0afcc2e9a3ff851a8bce88194391eb183b1bf66b
Moon Choi <choimy1991@gmail.com>
2022-06-08 10:57:12 -0700

    fix nil tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,28 +3,28 @@ require 'rails_helper'
 describe Product, type: :model do
   describe 'Validations' do
     it 'validates name field' do
-      params = { name: nil, description: 'hello', category_id: 1, quantity: 3, image: 'hello.jpg', price: 100 }
+      params = { description: 'hello', category_id: 1, quantity: 3, image: 'hello.jpg', price: 100 }
       @category = Category.new
       @product = Product.new params
       expect(@product.name).to be_nil
     end
 
     it 'validates price field' do
-      params = { name: 'hello', description: 'hello', category_id: 1, quantity: 3, image: 'hello.jpg', price: nil }
+      params = { name: 'hello', description: 'hello', category_id: 1, quantity: 3, image: 'hello.jpg' }
       @category = Category.new
       @product = Product.new params
-      expect(@product.price.fractional).to be 0
+      expect(@product.price).to be_nil
     end
 
     it 'validates quantity field' do
-      params = { name: 'hello', description: 'hello', category_id: 1, quantity: nil, image: 'hello.jpg', price: 100 }
+      params = { name: 'hello', description: 'hello', category_id: 1, image: 'hello.jpg', price: 100 }
       @category = Category.new
       @product = Product.new params
       expect(@product.quantity).to be_nil
     end
 
     it 'validates category field' do
-      params = { name: 'hello', description: 'hello', category_id: nil, quantity: 3, image: 'hello.jpg', price: 100 }
+      params = { name: 'hello', description: 'hello', quantity: 3, image: 'hello.jpg', price: 100 }
       @category = Category.new
       @product = Product.new params
       expect(@product.category).to be_nil
