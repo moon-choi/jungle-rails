2ffb2dfd2b970fa7861f66c99651629674e2286c
Moon Choi <choimy1991@gmail.com>
2022-06-08 11:31:37 -0700

    fix Model Spec: Product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,11 +2,21 @@ require 'rails_helper'
 
 describe Product, type: :model do
   describe 'Validations' do
+    it 'creates a new product' do
+      params = { name: 'hello', description: 'hello', category_id: 1, quantity: 3, image: 'hello.jpg', price: 100 }
+      @category = Category.new
+      @product = Product.new params
+      @product.save
+      expect(@product).not_to be_nil
+    end
+
     it 'validates name field' do
       params = { name: nil, description: 'hello', category_id: 1, quantity: 3, image: 'hello.jpg', price: 100 }
       @category = Category.new
       @product = Product.new params
+      @product.save
       expect(@product.name).to be_nil
+      expect(@product.errors.full_messages).not_to be_empty
     end
 
     # price excluded from params
@@ -14,21 +24,29 @@ describe Product, type: :model do
       params = { name: 'hello', description: 'hello', category_id: 1, quantity: 3, image: 'hello.jpg' }
       @category = Category.new
       @product = Product.new params
+      @product.save
       expect(@product.price).to be_nil
+      expect(@product.errors.full_messages).not_to be_empty
     end
 
     it 'validates quantity field' do
       params = { name: 'hello', description: 'hello', category_id: 1, quantity: nil, image: 'hello.jpg', price: 100 }
       @category = Category.new
       @product = Product.new params
+      @product.save
       expect(@product.quantity).to be_nil
+      expect(@product.errors.full_messages).not_to be_empty
+  
     end
 
     it 'validates category field' do
       params = { name: 'hello', description: 'hello', category_id: nil, quantity: 3, image: 'hello.jpg', price: 100 }
       @category = Category.new
       @product = Product.new params
+      @product.save
       expect(@product.category).to be_nil
+      expect(@product.errors.full_messages).not_to be_empty
+  
     end
   end
 end
