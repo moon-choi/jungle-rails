c41c27288c2e133d1b581d986ca7aaee42d1eb59
Moon Choi <choimy1991@gmail.com>
2022-06-08 10:58:29 -0700

    fix price nil test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 describe Product, type: :model do
   describe 'Validations' do
     it 'validates name field' do
-      params = { description: 'hello', category_id: 1, quantity: 3, image: 'hello.jpg', price: 100 }
+      params = { name: nil, description: 'hello', category_id: 1, quantity: 3, image: 'hello.jpg', price: 100 }
       @category = Category.new
       @product = Product.new params
       expect(@product.name).to be_nil
     end
 
+    # price excluded from params
     it 'validates price field' do
       params = { name: 'hello', description: 'hello', category_id: 1, quantity: 3, image: 'hello.jpg' }
       @category = Category.new
@@ -17,14 +18,14 @@ describe Product, type: :model do
     end
 
     it 'validates quantity field' do
-      params = { name: 'hello', description: 'hello', category_id: 1, image: 'hello.jpg', price: 100 }
+      params = { name: 'hello', description: 'hello', category_id: 1, quantity: nil, image: 'hello.jpg', price: 100 }
       @category = Category.new
       @product = Product.new params
       expect(@product.quantity).to be_nil
     end
 
     it 'validates category field' do
-      params = { name: 'hello', description: 'hello', quantity: 3, image: 'hello.jpg', price: 100 }
+      params = { name: 'hello', description: 'hello', category_id: nil, quantity: 3, image: 'hello.jpg', price: 100 }
       @category = Category.new
       @product = Product.new params
       expect(@product.category).to be_nil
