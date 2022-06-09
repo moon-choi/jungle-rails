1a13b87ede0fd04975fc51bb14e29871eaa2be7f
Moon Choi <choimy1991@gmail.com>
2022-06-08 22:04:10 -0700

    Feature Spec: Home Page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,12 +1,18 @@
-describe('example to-do app', () => {
+describe('visit homepage', () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
-    cy.visit('https://localhost:3000')
+    cy.visit('localhost:3000')
   })
 
   it('loads the homepage', () => {
+    cy.get("h2").should("contain", "Where you can find any plants!"); // Works!
   })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
 })
